6c2a2486a2b89d7a1ebce0d5b9406c4dcfea710a
Danil Morikov <danil.morikov@saritasa.com>
2023-08-25 22:33:26 +0500

    Move database project into src

--- MindBox.Products.Database/dbo/StoredProcedures/ProductsCategories_Group.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[ProductsCategories_Group]
-AS
-
-    SELECT p.[Name] as ProductName, c.[Name] as CategoryName FROM [dbo].[Products] p
-    LEFT JOIN [dbo].ProductsCategories pd ON p.Id = pd.ProductId
-    LEFT JOIN [dbo].Categories c ON c.Id = pd.CategoryId
-
-GO;
--- MindBox.Products.Database/dbo/Tables/Products.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Products]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(255) NOT NULL,
-)
